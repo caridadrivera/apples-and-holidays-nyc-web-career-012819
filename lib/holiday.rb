79f564ad4325b805dbc63d0a07e46ce362757d36
lively-teacher-9822 <ccrive10@gmail.com>
2019-01-27 01:01:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,23 +67,22 @@ def all_winter_holiday_supplies(holiday_hash)
       updated.flatten ## iterated in MULTIPLE ways and was only missing FLAT.
 end
 
+
+
+  
 def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
+   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
   #   New Years: Party Hats
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
-
-  holiday_hash.each do |season, data|
-    season = season.capitalize
-    season = season.to_s
-    data = data.to_s
-      puts "#{season}: #{data},"
-      data.each do |attribute, value|
-        str_holiday = holiday.to_s.split("_").collect {|word| word.capitalize!}.join(" ")
+  
+  holiday_hash.each do |season, holiday|
+    puts "#{season.capitalize}:"
+  holiday.each do |holiday, supplies|
+    puts "#{holiday.to_s.split("_").map {|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
     end
 
 end
